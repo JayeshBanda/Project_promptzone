d69e38b3eccdb101a91e06bfc72f03ab7ffce059
Jay <jayeshbanda4567@gmail.com>
2024-09-02 12:47:17 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import Feed from '@components/Feed'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='w-full flex-center flex-col'>
+        <h1 className='head_text text-center'>Discover & Share <br className='max-md:hidden'/>
+        <span className='orange_gradient tedxt-center'>AI-Powered Prompts
+            </span></h1>
+            <p className='desc text-center'>Promptzone is an open-source AI prompting tool for mordern world to discover, create and share creative prompts</p>
+            <Feed/>
+    </section>
+  )
+}
+
+export default Home
